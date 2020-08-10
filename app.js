20902df8a32771ee17aa894434f481c11e3e0cee
Isaac Chang <ichang1@haverford.edu>
2020-08-10 16:38:24 -0400

    made t block and drew it

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded',() => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+    //const height = 20
+    const score = document.querySelector('#score')
+    const start_pause = document.querySelector('#start-button')
 
+    const t_tetrimino = [
+        [0, 0, 0],
+        [0, 1, 0],
+        [1, 1, 1]
+    ]
+
+    let curPos = 3
+    function draw(tetrimino, initPos) {
+        tetrimino.forEach((row, y) => {
+            row.forEach((value,x) => {
+                if (value !== 0) {
+                    squares[curPos+10*y+x].classList.add('tetrimino')
+                }
+            })
+        })
+    }
+    function undraw(tetrimino, initPos) {
+        tetrimino.forEach((row, y) => {
+            row.forEach((value,x) => {
+                if (value !== 0) {
+                    squares[curPos+10*y+x].classList.remove('tetrimino')
+                }
+            })
+        })
+    }
+    draw(t_tetrimino,curPos)
 })
